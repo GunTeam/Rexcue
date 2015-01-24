7454ceaa2021fc7d6cf67e54a63315d7bc518234
Laura Breiman <lauracle@mit.edu>
2015-01-24 14:50:08 -0500

    artwork and first commit

---
+++ Rexscue.spritebuilder/GameScene.h
@@ -0,0 +1,16 @@
+//
+//  GameScene.h
+//  Rexscue
+//
+//  Created by Laura Breiman on 1/24/15.
+//  Copyright 2015 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameScene : CCNode {
+    
+}
+
+@end
---
+++ Rexscue.spritebuilder/GameScene.m
@@ -0,0 +1,14 @@
+//
+//  GameScene.m
+//  Rexscue
+//
+//  Created by Laura Breiman on 1/24/15.
+//  Copyright 2015 Apportable. All rights reserved.
+//
+
+#import "GameScene.h"
+
+
+@implementation GameScene
+
+@end
